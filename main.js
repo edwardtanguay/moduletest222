50f045418d3514518adfd990bf571b596598e67e
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-02 13:25:36 +0100

    getNumberOfCustomersInCity

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ import { orders } from './orders.js';
 
 // console.log(`There are ${customers.length} customers and ${orders.length} orders.`);
 
-console.log(`There are ${customers.filter(m => m.address.city === 'London').length} customers in London.`);
+const getNumberOfCustomersInCity = (city) => {
+	return `There are ${customers.filter(m => m.address.city === city).length} customers in ${city}.`;
+}
 
-console.log(`There are ${customers.filter(m => m.address.city === 'Berlin').length} customers in Berlin.`);
+// console.log(`There are ${customers.filter(m => m.address.city === 'London').length} customers in London.`);
 
+// console.log(`There are ${customers.filter(m => m.address.city === 'Berlin').length} customers in Berlin.`);
+
+console.log(getNumberOfCustomersInCity('London'));
+console.log(getNumberOfCustomersInCity('Berlin'));
+console.log(getNumberOfCustomersInCity('Nantes'));
+console.log(getNumberOfCustomersInCity('Paris'));
